97d4a2f113b899643148823ee5b39c41c33bf6a9
ogetatonny <tonnyogeta7598@gmail.com>
2024-02-15 22:49:36 +0300

    about, web

--- pages/about.py
+++ pages/about.py
@@ -1,3 +1,23 @@
 import streamlit as st
 
-st.write("Hello")
+st.write("In our daily interactions with students, friends or workmates, it is common to hear many "
+         "complaining about issues of time management. Either much time is taken in non productive activity"
+         "or too much time is taken on one activity than it was intended. It is also possible for these people to"
+         "totally forget about other activities/events lined up for the day.\n"
+         "\n"
+         "The todo web app is coming in to address this issue. We have enabled our users to key in their todo list"
+         "We have also curated user journey that allows our users to delete the todos they are no longer "
+         "interested with. It is a very simple to use app.\n"
+         ""
+         "It is worth noting that this is also a Portfolio Project for Holberton School. It is a project that "
+         "has a 4 weeks timeline. "
+         "Please the link to check "
+         "it out https://intranet.alxswe.com/projects/565 \n"
+         "\n"
+         "Team members:\n"
+         ""
+         "1. Tony Ogeta\n "
+                       "- Github: https://github.com/ogetatonny\n"
+                       "- Twitter: https://twitter.com/TonnyOgeta\n"
+                       "- LinkedIn: https://www.linkedin.com/in/tony-ogeta-ba566a143/\n"
+         "2. Edwin Omurwa\n")
