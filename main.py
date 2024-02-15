ce56229d17e6746d6e6b10d4f93bc60551e76b84
ogetatonny <tonnyogeta7598@gmail.com>
2024-02-15 22:17:10 +0300

    about, web

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ def add_todo():
 
 st.title("My Todo App")
 st.subheader("This is my todo app.")
-st.write("This app is to increase your productivity")
+st.write("This app is to increase our productivity")
 
 for index, todo in enumerate(todos):
     checkbox = st.checkbox(todo, key=todo)
---
+++ pages/Feature.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+st.write("This app contains the following features:\n"
+         "1. Checkbox - Clicking on the checkbox "
+         "deletes the todo item from the list\n"
+         "2. The todo list - These are the added todos. User adds them"
+         "by entering text on the input box below the todo list\n"
+         "3. The Input box - This is where our users enter their todo items.\n"
+         "4. Once the todo item is added on the input box, "
+         "user clicks on the enter key on their keyboard so that the todo "
+         "item is added to the todo item list")
